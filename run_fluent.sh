4312b14cf9add95e6a43d8326d365b05e9ef4856
upnere <154268962+upnere@users.noreply.github.com>
2025-11-27 17:14:11 +0200

    Add sh file

---
+++ run_fluent.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#PBS -N fluent_job
+#PBS -q batch
+#PBS -l walltime=00:10:00
+#PBS -l nodes=1:ppn=2,pmem=4g,feature=vasara
+#PBS -j oe
+
+INPUT_FILE="instruction.journal"
+
+echo "Node: `/bin/hostname`"
+echo "Time started: `date`"
+
+module load ansys/2023-R1
+
+cd $PBS_O_WORKDIR
+fluent 3d -g -t $PBS_NUM_PPN -mpi=ibmmpi -i $INPUT_FILE
+
+echo "Time ended: `date`"
